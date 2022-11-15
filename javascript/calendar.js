521b6d2ed0c0a3d20c9f847221a366a530056716
Bennett Biju Mathew <tekkit25637@gmail.com>
2022-11-15 22:49:39 +1100

    prototype..

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -4,27 +4,47 @@ var date = new Date;
 function displayCalendar(month, year) {    
     var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     const daysToMonth = new Date(year, month, 1).getDay();
-    const daysInMonth = new Date(year, month, 0).getDate();
-    const daysAfterMonth = 42 - (daysToMonth + daysInMonth);
-    
+    const daysInMonth = new Date(year, month+1, 0).getDate();
+    const daysAfterMonth = 43 - (daysToMonth + daysInMonth);
+
     var html = ""; 
 
     for (i = 0; i < days.length; i++) {
         html +=  `<div> ${days[i]} </div>`;
     }
 
-    for (i = 1; i < daysToMonth; i++) {
+    for (i = 0; i < daysToMonth-1; i++) {
         html +=  `<div class="day"> </div>`;
         document.getElementById('calendar').innerHTML = html;
     }
 
-    for (i = 0; i < daysInMonth+1; i++) {
+    for (i = 1; i < daysInMonth+1; i++) {
         if (i == date.getDate() && date.getMonth() == month && date.getFullYear() == year) {
-            html +=  `<div class="day"> <h3 style="color: red;"> ${i+1} </h3> </div>`;
+            html +=  `<div class="day"> <h3 style="color: red;"> ${i} </h3> </div>`;
         }
 
         else {
-            html +=  `<div class="day"> <h3> ${i+1} </h3> </div>`;
+            html +=  `<div class="day"> <h3> ${i} </h3>`;
+
+            for (k = 0; k < tasks["School"].length; k++) {
+                if (`${i}` == tasks["School"][k].dueDate) {
+                    html += `<p class="day-task" style="background-color: pink;"> ${tasks["School"][k].name} </p>`;
+                }
+            }
+
+            for (k = 0; k < tasks["A"].length; k++) {
+                if (`${i}` == tasks["A"][k].dueDate) {
+                    html += `<p class="day-task" style="background-color: beige;"> ${tasks["A"][k].name} </p>`;
+                }
+            }
+
+            for (k = 0; k < tasks["B"].length; k++) {
+                if (`${i}` == tasks["B"][k].dueDate) {
+                    html += `<p class="day-task" style="background-color: lightblue;"> ${tasks["B"][k].name} </p>`;
+                }
+            }
+
+            html +=  `</div>`;
         }
     }
 
--- javascript/tasks.js
+++ javascript/tasks.js
@@ -1,25 +1,23 @@
 var tasks = [];
-var html = ""
-var planners = ["A", "B", "C"];
+var html = "";
+var planners = ["School", "B", "A", "N/A"];
 var colors = ["pink", "beige", "lightblue",];
 
-tasks["A"] = [];
+tasks["N/A"] = [];
+tasks["School"] = [];
 tasks["B"] = [];
-tasks["C"] = [];
+tasks["A"] = [];
 
-tasks["C"].push(new Task("Finish Painting", "24", "need to see"));
-tasks["C"].push(new Task("Finish Painting", "24", "need to see"));
-tasks["C"].push(new Task("Finish Painting", "31", "need to see"));
-tasks["C"].push(new Task("Finish Painting", "31", "need to see"));
-tasks["C"].push(new Task("Finish Painting", "31", "need to see"));
-tasks["A"].push(new Task("Scam", "28/05/10", "need to see"));
-tasks["A"].push(new Task("Scam", "28/05/10", "need to see"));
-tasks["A"].push(new Task("Scam", "28/05/10", "need to see"));
-tasks["B"].push(new Task("Taxi", "24/15/10", "need to see"));
-tasks["B"].push(new Task("Taxi", "24/15/10", "need to see"));
-tasks["B"].push(new Task("Taxi", "24/15/10", "need to see"));
-tasks["B"].push(new Task("Taxi", "24/15/10", "need to see"));
-tasks["B"].push(new Task("Taxi", "24/15/10", "need to see"));
+tasks["School"].push(new Task("Accounting Exam", "8", "2:00 - 4:15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
+tasks["School"].push(new Task("Computing Exam", "11", "3.00 - 5.15pm"));
 
 function Task(name, dueDate, details) {
     this.name = name;
@@ -30,19 +28,38 @@ function Task(name, dueDate, details) {
 function displayTasks(planner, color) {
     for (i = 0; i < tasks[planner].length; i++) {
         html += `<aside class="task"> 
-                    <button class="task-button" style="background-color: ${color}"> test </button>
+                    <button class="task-button" style="background-color: ${color}"> </button>
                     <div class="task-text">
-                        <h3> ${tasks[planner][0].name} </h3>
-                        <p> ${planner}, ${tasks[planner][0].dueDate}, ${tasks[planner][0].details} </p>                    
+                        <h3> ${tasks[planner][i].name} </h3>
+                        <p> ${planner}, ${tasks[planner][i].dueDate}, ${tasks[planner][i].details} </p>                    
                     </div>
                 </aside>`
+        document.getElementById('list').innerHTML = html;
     }
 
-    document.getElementById('list').innerHTML = html;
+    html = "";
 }
 
 function run() {
     for (j = 0; j < planners.length; j++) {
         displayTasks(planners[j], colors[j]);
+    }
+}
+
+let size = 0;
+for (key in tasks) {
+     if (tasks.hasOwnProperty(key)) {
+        size++;
+        console.log(size);
+    }
+}
+
+function addTask(input) {
+    if (event.key == 'Enter') {
+        var name = document.getElementById(input).value;
+
+        tasks["N/A"].push(new Task(name, "", ""));
+        
+        displayTasks("School", colors[0]);
     }
 }
